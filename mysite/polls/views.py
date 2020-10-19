701e772fe14ae693975d6a1b58c6ca896849d4ac
Lisa <onionmccabbage@live.com>
2020-10-19 16:29:43 +0100

    kjhlkj

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 # Create your views here.
 from .models import Question
@@ -6,7 +6,7 @@ from .models import Question
 # it's a good idea to pass sensible defaults to views
 def index(request): # by convention the home page is called index
     # iterate over every member in our question collection
-    latest_question_list = Question.objects.order_by('-pub_date')
+    latest_question_list = Question.objects.order_by('-pub_date')[:3]
     # output = ', '.join([q.question_text for q in latest_question_list])
     # return HttpResponse(output)
     # better way - use the 'render' shortcut
@@ -14,9 +14,12 @@ def index(request): # by convention the home page is called index
     #     original request, template, context
     return render(request, 'polls/index.html', context)
 
-
+# we an use our 404 shortcut here
 def detail(request, question_id=1): # we can pass a parameter as 'question_id'
-    return HttpResponse("Here's detals of question {}".format(question_id) ) # we can pass parameters
+    # recommend using get_object_or_404
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question':question})
+    # return HttpResponse("Here's detals of question {}".format(question_id) ) # we can pass parameters
 
 def results(request, question_id=1):
     return HttpResponse('Results for question {}'.format(question_id) )
