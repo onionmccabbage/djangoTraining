934ef1843ec84fa8a069009ea7f6310c03b5838f
Lisa <onionmccabbage@live.com>
2020-10-21 11:12:06 +0100

    admin custom

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -5,6 +5,25 @@ from .models import Question
 from .models import Weather
 from .models import Choice
 
-admin.site.register(Question)
+# delare a class to 'inline' the choices along with the question
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    # we can choose how many to show by default
+    extra = 3
+
+# declare a class to customize the Question admin
+class QuestionAdmin(admin.ModelAdmin):
+    # we can specify the order of the form fields
+    # fields = ['pub_date', 'question_text']
+    # .. or group fields together in field sets
+    fieldsets= [
+                (None, {'fields':['question_text']}),
+                ('Date info', {'fields':['pub_date'], 'classes':['collapse']}),
+               ]
+    inlines = [ChoiceInline]
+
+
+admin.site.register(Question, QuestionAdmin) # register our admin class
 admin.site.register(Weather)
-admin.site.register(Choice)
+# We no longer register 'choice' as a separate form
+# admin.site.register(Choice)
